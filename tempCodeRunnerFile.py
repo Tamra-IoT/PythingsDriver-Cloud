fbed8467eec40bf98037a55fdce738e706d46db0
elewah <abdelrahman.elewah@gmail.com>
2023-09-23 23:31:41 -0400

    fixing long commands frame issue

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-things.append(tamra_thing("Blue LED","this LED can be turned on and turned off",D2, json_settings).jsonFile)
-# things.append(tamra_thing("black LED","this LED can be turned on and turned off",D2, json_settings).jsonFile)
+import os
